043a1fadc0f1238cdb8f0267b4135bd984187bf0
Sandra Blanco <sandra.blanco18@gmail.com>
2026-03-07 11:42:54 +0100

    updating Coin.jsx,Root.jsx and Router.jsx

--- src/pages/Coin.jsx
+++ src/pages/Coin.jsx
@@ -1,17 +1,18 @@
 import {useState, useEffect} from 'react';
 import {useParams} from 'react-router-dom';
- const API_KEY = import.meta.env.VITE_API_KEY;
+const API_KEY = import.meta.env.VITE_API_KEY;
 
 function Coin() {
     const [coin, setCoin] = useState(null);
     const {id} = useParams();
     useEffect(() => {
-        fetch(`https://rest.coincap.io/v3/assets/${id}?apiKey=${import.meta.env.VITE_API_KEY}`)
+        fetch(`https://rest.coincap.io/v3/assets/${id}?apiKey=${API_KEY}`)
             .then((response) => response.json())
             .then((data) => setCoin(data.data));
     }   , [id]);
     //funcion para añadir a favoritos
     const addFavorites = () => {
+        if (!coin) return; //si no hay moneda no hacemos nada
         const storedFavorites = localStorage.getItem('favorites');//Lee favs guardadps
         const favorites = storedFavorites ? JSON.parse(storedFavorites) : [];//si hay favs lo convertimos en array
         if (!favorites.includes(coin.id)) { //si no esta el id de la moneda en favs lo añadimos
@@ -20,26 +21,29 @@ function Coin() {
         }
     };
     const deleteFavorites = () => {
+        if (!coin) return; 
         const storedFavorites = localStorage.getItem('favorites');//lee los favs guardados
         const favorites = storedFavorites ? JSON.parse(storedFavorites) : [];//Si hay favs lo convertimos en  array
         const updatedFavorites = favorites.filter((favId) => favId !== coin.id); //filtramos el array para eliminar el id de la moneda
         localStorage.setItem('favorites', JSON.stringify(updatedFavorites));//actualizamos el localStorage con el array actualizado sin la moneda eliminada
     };
+    
     if (!coin) {
-        return (
-            <p>Loading... </p>)}
-            return (
-        <div>
+    return <p>Loading...</p>;
+}
+
+return (
+    <div>
         <p>{coin.name}</p>
         <p>Rank: {coin.rank}</p>
         <p>Symbol: {coin.symbol}</p>
         <p>Price (USD): ${parseFloat(coin.priceUsd).toFixed(2)}</p>
         <button onClick={addFavorites}>Add to Favorites</button>
         <button onClick={deleteFavorites}>Delete from Favorites</button>
-        </div>
-        );
+    </div>
+);
 
     }
-}
+
 
 export default Coin;
--- src/pages/Root.jsx
+++ src/pages/Root.jsx
@@ -3,11 +3,12 @@ import {Link, Outlet} from 'react-router-dom';
 function Root() {
     return (
         <>
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/Favorites">Favorites</Link>
-            <Outlet /> {/*activa las rutas hijas*/}
-        </div>
+        <nav>
+            <Link to="/">Home</Link> | 
+            <Link to="/favorites"> Favorites</Link>
+           
+        </nav>
+         <Outlet /> {/*activa las rutas hijas*/}
         </>
     );
 }
